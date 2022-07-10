3465eeed8c0db2c65ace6fd4f3877543ac41011a
lroa28 <lucreciaroa@hotmail.com>
2022-07-10 01:17:55 -0300

    commit 10/07

--- DesafiosEntregables/11may12maClase/server.js
+++ DesafiosEntregables/11may12maClase/server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 
-
 const { Server: HttpServer } = require('http')
 const { Server: IOServer } = require('socket.io')
 
@@ -8,22 +7,21 @@ const app = express()
 const httpServer = new HttpServer(app)
 const io = new IOServer(httpServer)
 
-   //...
-    // Indicamos que queremos cargar los archivos estáticos que se encuentran en dicha carpeta
-    app.use(express.static('public'))
-    // Esta ruta carga nuestro archivo index.html en la raíz de la misma
-    app.get('/', (req, res) => {
-        res.sendFile('index.html', { root: __dirname })
-    })
-    // El servidor funcionando en el puerto 3000
-    httpServer.listen(3000, () => console.log('SERVER ON'))
+// Indicamos que queremos cargar los archivos estáticos que se encuentran en dicha carpeta
+app.use(express.static('public'))
 
-     //...
-  io.on('connection', (socket) => {
-    // "connection" se ejecuta la primera vez que se abre una nueva conexión
-      console.log('Usuario conectado')
-    // Se imprimirá solo la primera vez que se ha abierto la conexión    
-    })
+// Esta ruta carga nuestro archivo index.html en la raíz de la misma
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname })
+})
+
+
+//...
+io.on('connection', (socket) => {
+// "connection" se ejecuta la primera vez que se abre una nueva conexión
+  console.log('Usuario conectado')
+// Se imprimirá solo la primera vez que se ha abierto la conexión    
+})
 
  // Servidor
  io.on('connection', socket => {
@@ -50,12 +48,28 @@ socket.on('mi mensaje', data => {
 io.on('connection', socket => {
     console.log('¡Nuevo cliente conectado!');
 
-    /* Envio los mensajes al cliente que se conectó*/
-    socket.emit('mensajes', mensajes);
+/* Envio los mensajes al cliente que se conectó*/
+socket.emit('mensajes', mensajes);
 
-    /*Escucho los mensajes enviados por el cliente y se los propago a todos*/
-    socket.on('mensaje', data => {
-        mensajes.push({ socketid: socket.id, mensaje: data })
-        io.sockets.emit('mensajes', mensajes);
-    });
+/*Escucho los mensajes enviados por el cliente y se los propago a todos*/
+socket.on('mensaje', data => {
+    mensajes.push({ socketid: socket.id, mensaje: data })
+    io.sockets.emit('mensajes', mensajes);
 });
+});
+
+// El servidor funcionando en el puerto 3000
+// httpServer.listen(3000, () => console.log('SERVER ON'))
+
+// conexión standar para el local host
+const PORT = 8080 || process.env.PORT;
+const connectedServer = httpServer.listen(PORT, function () {
+  console.log(
+    `Servidor Http con Websockets escuchando en el puerto ${connectedServer.address().port
+    }`
+  );
+});
+
+connectedServer.on("error", (error) =>
+  console.log(`Error en servidor ${error}`)
+);
